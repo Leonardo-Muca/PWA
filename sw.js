8e875d56d3ae61446ec68e9b29c034c2e0d10cec
Leonardo <leonardo.munoz@xaldigital.com>
2022-11-30 19:02:28 -0600

    Ya sirve

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
 const STATIC_CACHE = "static-v1";
 
-const APP_SHELL = [
-  "/",
-  "index.html",
-  "css/styles.css",
-  "js/main.js",
-  "images/beerjs-logo.png",
-  "images/1.png",
-  "images/2.png",
-  "images/3.png",
-  "images/facebook.png",
-  "images/twiter.png",
-  "images/instagram.png",
-];
+// const APP_SHELL = [
+//   "/",
+//   "index.html",
+//   "css/styles.css",
+//   "js/main.js",
+//   "images/beerjs-logo.png",
+//   "images/1.png",
+//   "images/2.png",
+//   "images/3.png",
+//   "images/facebook.png",
+//   "images/twiter.png",
+//   "images/instagram.png",
+// ];
 
-self.addEventListener("install", (e) => {
-  const cacheStatic = caches
-    .open(STATIC_CACHE)
-    .then((cache) => cache.addAll(APP_SHELL));
+// self.addEventListener("install", (e) => {
+//   const cacheStatic = caches
+//     .open(STATIC_CACHE)
+//     .then((cache) => cache.addAll(APP_SHELL));
 
-  e.waitUntil(cacheStatic);
-});
+//   e.waitUntil(cacheStatic);
+// });
 
 self.addEventListener("fetch", (e) => {
   console.log("fetch! ", e.request);
